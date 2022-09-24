0a582bcd8be0ae9926ac37471e6e7ed655ff98d7
unknown <vuuthongnguyen@gmail.com>
2022-09-24 15:55:50 +0700

    final

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,13 +1,16 @@
 import bg from '../assets/bg.png';
-import chef from '../assets/chef.png';
+import chef from '../assets/chef.jpg';
 import G from '../assets/G.png';
-import gallery01 from '../assets/gallery01.png';
-import gallery02 from '../assets/gallery02.png';
-import gallery03 from '../assets/gallery03.png';
-import gallery04 from '../assets/gallery04.png';
-import knife from '../assets/knife.png';
+import gallery01 from '../assets/gallery01.jpg';
+import gallery02 from '../assets/gallery02.jpg';
+import gallery03 from '../assets/gallery03.jpg';
+import gallery04 from '../assets/gallery04.jpg';
+import gallery05 from '../assets/gallery05.jpg';
+import gallery06 from '../assets/gallery06.jpg';
+import gallery07 from '../assets/gallery07.jpg';
+import knife from '../assets/knife.jpg';
 import logo from '../assets/logo.png';
-import menu from '../assets/menu.png';
+import menu from '../assets/menu.jpg';
 import overlaybg from '../assets/overlaybg.png';
 import spoon from '../assets/spoon.svg';
 import welcome from '../assets/welcome.png';
@@ -29,6 +32,9 @@ export default {
   gallery02,
   gallery03,
   gallery04,
+  gallery05,
+  gallery06,
+  gallery07,
   knife,
   logo,
   menu,
